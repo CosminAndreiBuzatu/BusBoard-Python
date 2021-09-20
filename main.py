748375003e71c347598a6c5d51c6b8aae8745d6c
cosmin.buzatu <cosmin.buzatu@altran.com>
2021-09-20 15:54:32 +0100

    Display next 5 buses that will stop at a given station

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import requests
+from constants import *
+
+def getInfoBusStation(atcoCode):
+    r = requests.get(f'https://transportapi.com/v3/uk/bus/stop/{atcoCode}/live.json?app_id={appId}&app_key={key}&group=no&limit=5&nextbuses=yes')
+    response = r.json()
+    return response
+def departureBusesInfo(responseApi):
+    departures = responseApi['departures']['all']
+    for busNumber in departures:
+        print('Bus number ' +busNumber['line'] + ' from ' + busNumber['direction'] + ' expected to arrive ' + busNumber['expected']['arrival']['time'])
 def main():
-    print("Welcome to BusBoard.")
-
-
-if __name__ == "__main__": main()
+    busStop = input('Type a bus stop code: ')
+    apiResponse = getInfoBusStation(busStop)
+    departureBusesInfo(apiResponse)
+if __name__ == "__main__":
+    main()
