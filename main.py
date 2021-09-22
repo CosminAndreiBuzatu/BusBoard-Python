f24fcdb4e7da076254b9da5d691213c50372146e
cosmin.buzatu <cosmin.buzatu@altran.com>
2021-09-22 10:08:47 +0100

    Search by postcode 2 nearest bus stops and display 5 buses that will arrive(OOP)

--- main.py
+++ main.py
@@ -1,70 +1,106 @@
 import requests
 from key import *
-printStatements =[]
-def getInfoBusStation(atcoCode):
-    actocodes = []
-    for atco in atcoCode:
-        r = requests.get(f'https://transportapi.com/v3/uk/bus/stop/{atco}/live.json?app_id={appId}&app_key={key}&group=no&limit=5&nextbuses=yes')
+
+printStatements = []
+
+
+class PostCode:
+
+
+    def __init__(self, postcode):
+        self.postcode = postcode
+
+    def getPostcodeInfo(self):
+        r = requests.get(f'https://api.postcodes.io/postcodes/{self.postcode}')
         response = r.json()
-        actocodes.append(response)
-    return actocodes
+        return response
+
+    def getCoordonate(self):
+        r = self.getPostcodeInfo()
+        longitude = r['result']['longitude']
+
+        latitude = r['result']['latitude']
+        return longitude, latitude
 
 
-def departureBusesInfo(responseApi):
-    for busStops in responseApi:
-        departures = busStops['departures']['all']
+class BusStop:
+
+
+    def __init__(self, longitude, latitude):
+          self.longitude = longitude
+          self.latitude = latitude
+
+
+    def getBusStations(self):
+        r = requests.get(f'https://transportapi.com/v3/uk/places.json?app_id={appId}&app_key={key}&lat={self.latitude}&lon={self.longitude}')
+        response = r.json()
+        return response
+
+
+    def getTwoNearestBusStops(self):
+        busStations = self.getBusStations()
+        busStops = busStations['member']
         count = 0
-        for busNumber in departures:
-            if busNumber['aimed_departure_time'] == None and count < 5:
-                printStatements.append('Bus number ' + busNumber['line'] + ' from ' + busNumber['direction'] + ' expected to arrive Uknown')
-            if count < 5:
-                printStatements.append('Bus number ' + busNumber['line'] + ' from ' + busNumber['direction'] + ' expected to arrive ' + busNumber['aimed_departure_time'])
-            count +=1
-
-def getPostcodeInfo(postcode):
-    r = requests.get(f'https://api.postcodes.io/postcodes/{postcode}')
-    response = r.json()
-    return response
+        atcocode = []
+        for busStop in busStops:
+            if count < 2 and busStop['type'] == 'bus_stop':
+                printStatements.append('Bus stop "' + busStop['name'] + '" the distance from your postcode to the bus - ' + str(busStop['distance']))
+                count += 1
+                atcocode.append(busStop['atcocode'])
+        return atcocode
 
 
-def coordonateInfo(responseApi):
-    longitude = responseApi['result']['longitude']
-    latitude = responseApi['result']['latitude']
-    return longitude,latitude
+    def getInfoBusStation(self):
+        actocodes = []
+        busStopSerials = self.getTwoNearestBusStops()
+        for busStopSerial in busStopSerials:
+            r = requests.get(f'https://transportapi.com/v3/uk/bus/stop/{busStopSerial}/live.json?app_id={appId}&app_key={key}&group=no')
+            response = r.json()
+            actocodes.append(response)
+        return actocodes
 
 
-def getNearBusStations(coord):
-    r = requests.get(f'https://transportapi.com/v3/uk/places.json?app_id={appId}&app_key={key}&lat={coord[1]}&lon={coord[0]}')
-    response = r.json()
-    return response
+    def getDepartureBusesInfo(self):
+        busStops = self.getInfoBusStation()
+        for buses in busStops:
+            departures = buses['departures']['all']
+            count = 0
+            for busNumber in departures:
+                if busNumber['aimed_departure_time'] == None and count < 5:
+                    printStatements.append('Bus number ' + busNumber['line'] + ' from ' + busNumber['direction'] + ' expected to arrive Uknown')
+                if count < 5:
+                    printStatements.append('Bus number ' + busNumber['line'] + ' from ' + busNumber['direction'] + ' expected to arrive ' + busNumber['aimed_departure_time'])
+                count += 1
+        return printStatements
 
 
-def twoNearestBusStops(responseApi):
-    count = 0
-    atcocode = []
-    busStops = responseApi['member']
-    for bus in busStops:
-        if count < 2 and bus['type'] == 'bus_stop':
-            printStatements.append('Bus stop "' + bus['name'] + '" the distance from your postcode to the bus - ' + str(bus['distance']))
-            count += 1
-            atcocode.append(bus['atcocode'])
-    return atcocode
+class Display:
 
 
-def dispay(y):
-            print(y[0] + ' \n The next 5 buses at this station are:  \n' + y[2] + '\n' + y[3] + '\n' + y[4] + '\n' + y[5] + '\n' + y[6]+ ' \n')
-            print(y[1] + ' \n The next 5 buses at this station are:  \n' + y[7] + '\n' + y[8] + '\n' + y[9] + '\n' + y[10] + '\n' + y[11])
+    def __init__(self, data):
+        self.data = data
+
+
+    def display(self):
+        print(self.data[0] + ' \n\n The next 5 buses at this station are:  \n\n' + self.data[2] + '\n' + self.data[3] + '\n' + self.data[4] + '\n' + self.data[5] + '\n' + self.data[6] + ' \n')
+        print(self.data[1] + ' \n\n The next 5 buses at this station are:  \n\n' + self.data[7] + '\n' + self.data[8] + '\n' + self.data[9] + '\n' + self.data[10] + '\n' + self.data[11])
 
 
 def main():
-    postcode = input('Type a postcode: ')
-    postApi = getPostcodeInfo(postcode)
-    coord = coordonateInfo(postApi)
-    x = getNearBusStations(coord)
-    atcoCodes = twoNearestBusStops(x)
-    y =getInfoBusStation(atcoCodes)
-    departureBusesInfo(y)
-    dispay(printStatements)
+
+    inputPostcode = input('Type a postcode: ')
+
+    postcode1 = PostCode(inputPostcode)
+
+    lat , lon = postcode1.getCoordonate()
+
+    postcodeBuses = BusStop(lat , lon)
+
+    data = postcodeBuses.getDepartureBusesInfo()
+
+    display1 = Display(data)
+
+    display1.display()
 
 
 if __name__ == "__main__":
